c35b7cf9996be9d733769dfdd439a75c58d24664
RNGaming10 <iytityity04@gmail.com>
2024-09-01 20:35:25 +0200

    Added Drill to the item group

--- src/main/java/qa/luffy/pseudo/common/init/PseudoCreativeTabs.java
+++ src/main/java/qa/luffy/pseudo/common/init/PseudoCreativeTabs.java
@@ -26,6 +26,7 @@ public class PseudoCreativeTabs {
         pOutput.accept(PseudoItems.IRON_GEAR);
         //advanced items
         pOutput.accept(PseudoItems.CHAINSAW);
+        pOutput.accept(PseudoItems.DRILL);
     }).build());
 
     public static final Supplier<CreativeModeTab> PSEUDO_BLOCKS_TAB = CREATIVE_MODE_TABS.register("pseudo_blocks_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.pseudo.pseudo_blocks_tab")).icon(() -> new ItemStack(PseudoBlocks.RAW_GRAPHITE_BLOCK)).withTabsBefore(ResourceLocation.fromNamespaceAndPath(Pseudo.MODID, "pseudo_items_tab")).displayItems((pParameters, pOutput) -> {
